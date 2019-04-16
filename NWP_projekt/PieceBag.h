9b4be2ea0a1d82e8d298de280c9791bd49ba98b9
Petar Domitrović <domitrovicpetar@gmail.com>
2019-04-16 18:20:19 +0200

    "Breakpoint"

--- NWP_projekt/PieceBag.h
+++ NWP_projekt/PieceBag.h
@@ -4,6 +4,7 @@ class PieceBag
 {
 public:
 	Piece current_piece = { {0, 0}, true, 0, 0 };
+	Piece last_piece = { {0, 0}, true, 0, 0 };
 	std::vector<Piece> pieces;
 	void Initialize();
 	bool CheckField(POINT field);
